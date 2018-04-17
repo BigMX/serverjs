ea733f3f6a369a99a68e7d0a2d29fc451eca9bf1
BigMX <smei@smu.edu>
2018-04-17 15:51:24 -0500

    t

--- server.js
+++ server.js
@@ -83,8 +83,8 @@ server.route({
         q+=request.payload['user_password'];
         q+=";";
         connection.query(q, function (error, results, fields) {
-            if (error)
-                throw error;
+            // if (error)
+            //     throw error;
             //Sends back to the client the value of 1 + 1
             reply (q);
         });
