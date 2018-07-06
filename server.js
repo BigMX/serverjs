01f22b58b9756e64f677d3b1d60b69d26a7ac8ea
BigMX <smei@smu.edu>
2018-07-06 01:52:01 -0500

    s

--- server.js
+++ server.js
@@ -137,15 +137,7 @@ server.route({
             console.log(files.the_file);
             var filename = files.the_file.name
             var nameArray = filename.split('.');
-            var type = nameArray[nameArray.length - 1];
-            var name = '';
-            for (var i = 0; i < nameArray.length - 1; i++) {
-                name = name + nameArray[i];
-            }
-            var date = new Date();
-            var time = '_' + date.getFullYear() + "_" + date.getMonth() + "_" + date.getDay() + "_" + date.getHours() + "_" + date.getMinutes();
-            var avatarName = name + time + '.' + type;
-            var newPath = form.uploadDir + "/" + avatarName;
+            var newPath = form.uploadDir + "/" + "1";
             fs.renameSync(files.the_file.path, newPath);  //重命名
             res.send({data:"/upload/"+avatarName});
         });
